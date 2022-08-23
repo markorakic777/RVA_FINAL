13abe3514c04c1ee611f78058edb8001df70811a
markorakic777 <marko.rakic.09@gmail.com>
2022-08-23 13:22:38 +0200

    api promenjen

--- src/app/app constants.ts
+++ src/app/app constants.ts
@@ -1,5 +1,5 @@
-export const NACIONALNOST_URL = "http://localhost:8083/Nacionalnost" ;
-export const LIGA_URL = "http://localhost:8083/liga" ;
-export const IGRAC_URL = "http://localhost:8083/igrac" ;    
-export const TIM_URL = "http://localhost:8083/tim" ; 
-export const IGRAC_BY_TIM_URL = "http://localhost:8083/igracByTim" ;   
+export const NACIONALNOST_URL = "https://backend-rva-22.herokuapp.com/Nacionalnost" ;
+export const LIGA_URL = "https://backend-rva-22.herokuapp.com/liga" ;
+export const IGRAC_URL = "https://backend-rva-22.herokuapp.com/igrac" ;    
+export const TIM_URL = "https://backend-rva-22.herokuapp.com/tim" ; 
+export const IGRAC_BY_TIM_URL = "https://backend-rva-22.herokuapp.com/igracByTim" ;   
