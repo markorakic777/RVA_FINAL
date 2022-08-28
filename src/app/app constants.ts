ea716456422ad16deadfaf24d46bdd80f0548a2b
markorakic777 <marko.rakic.09@gmail.com>
2022-08-28 20:30:19 +0200

    master

--- src/app/app constants.ts
+++ src/app/app constants.ts
@@ -1,5 +1,5 @@
-export const NACIONALNOST_URL = "https://backend-rva-22.herokuapp.com/Nacionalnost" ;
-export const LIGA_URL = "https://backend-rva-22.herokuapp.com/liga" ;
-export const IGRAC_URL = "https://backend-rva-22.herokuapp.com/igrac" ;    
-export const TIM_URL = "https://backend-rva-22.herokuapp.com/tim" ; 
-export const IGRAC_BY_TIM_URL = "https://backend-rva-22.herokuapp.com/igracByTim" ;   
+export const NACIONALNOST_URL = "https://bekendzaprojekarvamarkorakicit.herokuapp.com/Nacionalnost" ;
+export const LIGA_URL = "https://bekendzaprojekarvamarkorakicit.herokuapp.com/liga" ;
+export const IGRAC_URL = "https://bekendzaprojekarvamarkorakicit.herokuapp.com/igrac" ;    
+export const TIM_URL = "https://bekendzaprojekarvamarkorakicit.herokuapp.com/tim" ; 
+export const IGRAC_BY_TIM_URL = "https://bekendzaprojekarvamarkorakicit.herokuapp.com/igracByTim" ;   
